8b5662676a8f5e03ef6942b21758caf6d2ccd444
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-04 14:12:35 +0300

    Done.

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const employee = {
     streetAddress:'11 broadway'
 }
 function updateEmployeeWithKeyAndValue(employee,key,value){
-    return Object.assign({},employee, {[key]:value});
+    return {...employee,[key]:value};
 
 }
-function destructiveUpdateEmployeeWithKeyAndValue(employee,key,value){
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
     employee[key]= value;
     return employee;
 }
-function deleteFromEmployeeByKey(driver, key){
+function deleteFromEmployeeByKey(employee, key){
     const newObj = Object.assign({},employee)
     delete newObj[key];
     return newObj
 }
-function destructiveDeleteEmployeeWithKeyAndValue(driver,key){
+function destructivelyDeleteFromEmployeeByKey(employee,key){
     delete employee[key];
     return employee
 }
